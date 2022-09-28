6685f44a7ddb6d926ad66048e1e76cda7b02a4a2
baripembo <erikawei@gmail.com>
2022-09-28 15:53:23 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1386,7 +1386,8 @@ function createMapTooltip(country_code, country_name, point) {
     var tableArray = [{label: 'Population', indicator: '#population'},
                       {label: 'Population in IPC Phase 3+', indicator: '#affected+food+ipc+p3plus+num'},
                       {label: 'People Affected', indicator: '#affected+total'},
-                      {label: 'People Targeted', indicator: '#targeted+total'}];
+                      {label: 'People Targeted', indicator: '#targeted+total'},
+                      {label: 'People Reached', indicator: '#reached+total'}];
     content += '<div class="table-display">';
     tableArray.forEach(function(row) {
       if (row.indicator!=currentIndicator.id) {
@@ -1440,7 +1441,8 @@ function createCountryMapTooltip(name, pcode, point) {
     var tableArray = [{label: 'Population', indicator: '#population'},
                       {label: 'Population in IPC Phase 3+', indicator: '#affected+food+ipc+p3plus+num'},
                       {label: 'People Affected', indicator: '#affected+total'},
-                      {label: 'People Targeted', indicator: '#targeted+total'}];
+                      {label: 'People Targeted', indicator: '#targeted+total'},
+                      {label: 'People Reached', indicator: '#reached+total'}];
 
     //show ipc phase for KEN only
     if (currentCountry.code=='KEN') {
