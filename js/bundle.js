081eaee7b275d75f93923300ffbee56fd98d0e8b
baripembo <erikawei@gmail.com>
2022-10-05 12:41:10 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1083,7 +1083,7 @@ function loadIPCLayer(country) {
         '#640100'
       ]
     }
-  }, subnationalLabelLayer);
+  }, baseLayer);
 
   map.addLayer({
     id: `${country.iso}-ipc-boundary-layer`,
@@ -1092,7 +1092,7 @@ function loadIPCLayer(country) {
     paint: {
       'line-color': '#E0E0E0',
     }
-  }, subnationalLabelLayer);
+  }, baseLayer);
 
   map.addLayer({
     id: `${country.iso}-ipc-label-layer`,
@@ -1111,7 +1111,7 @@ function loadIPCLayer(country) {
       'text-halo-width': 1,
       'text-halo-blur': 1
     }
-  }, subnationalLabelLayer);
+  }, baseLayer);
 
   map.on('mouseenter', `${country.iso}-ipc-layer`, onMouseEnter);
   map.on('mouseleave', `${country.iso}-ipc-layer`, onMouseLeave);
