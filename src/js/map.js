10957f2a42150d3f27d4417c144155256bb8d58e
baripembo <erikawei@gmail.com>
2022-10-11 13:41:02 -0400

    hiding mapbox labels on ipc layers

--- src/js/map.js
+++ src/js/map.js
@@ -567,4 +567,13 @@ function toggleIPCLayers(visible, currCountry) {
     map.setLayoutProperty(`${country.iso}-ipc-boundary-layer`, 'visibility', vis);
     map.setLayoutProperty(`${country.iso}-ipc-label-layer`, 'visibility', vis);
   });
+
+  if (visible) {
+    map.setLayoutProperty(globalLabelLayer, 'visibility', 'none');
+    map.setLayoutProperty(subnationalLabelLayer, 'visibility', 'none');
+  }
+  else {
+    map.setLayoutProperty(globalLabelLayer, 'visibility', 'visible');
+    map.setLayoutProperty(subnationalLabelLayer, 'visibility', 'visible');
+  }
 }
