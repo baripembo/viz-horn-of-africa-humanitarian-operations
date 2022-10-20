4c251970e9efecc7fd4a0993b88ce611f8f690bf
baripembo <erikawei@gmail.com>
2022-10-20 13:27:12 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1266,7 +1266,7 @@ function updateCountrySource() {
 }
 
 function zoomToRegion() {
-  var offset = 50;
+  var offset = 100;
   let mapPadding = (isMobile) ?
     {
         top: 0,
@@ -1276,9 +1276,9 @@ function zoomToRegion() {
     } :
     { 
       top: offset,
-      right: 0,
+      right: offset,
       bottom: offset,
-      left: $('.key-figure-panel').outerWidth() - offset,
+      left: $('.key-figure-panel').outerWidth(),
     };
   map.fitBounds(regionBoundaryData[0].bbox, {
     padding: {top: mapPadding.top, right: mapPadding.right, bottom: mapPadding.bottom, left: mapPadding.left},
