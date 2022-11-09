d6ad895323c94b401cb0eaab8376559d8d5f1094
baripembo <erikawei@gmail.com>
2022-11-09 17:45:04 -0500

    add tracking to tab events

--- src/js/map.js
+++ src/js/map.js
@@ -519,6 +519,7 @@ function createEvents() {
     var selected = d3.select(this).node().value;
     if (selected!='') {
       updateRanking(selected);
+      vizTrack(`chart view`, selected);
     }
   });
 
