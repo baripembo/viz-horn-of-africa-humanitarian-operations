628893f15d078e37db04463d1a955ed4f90441fb
baripembo <erikawei@gmail.com>
2022-12-19 15:11:06 -0500

    debug

--- src/js/map.js
+++ src/js/map.js
@@ -282,6 +282,7 @@ function loadRasters() {
 }
 
 function loadIPCLayer(country) {
+  console.log(country.data)
   map.addSource(`${country.iso}-ipc`, {
     type: 'geojson',
     data: country.data,
